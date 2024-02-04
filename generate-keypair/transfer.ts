c0f601a695d03c220eee22b92b27f9356c8fca42
RsdNoob <60094708+RsdNoob@users.noreply.github.com>
2024-02-04 20:02:06 +0800

    added transfer script

---
+++ generate-keypair/transfer.ts
@@ -0,0 +1,62 @@
+import {
+    Connection,
+    Transaction,
+    SystemProgram,
+    sendAndConfirmTransaction,
+    PublicKey,
+  } from "@solana/web3.js";
+  import "dotenv/config";
+  import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+  
+  const main = async () => {
+    const suppliedToPubkey = process.argv[2] || null;
+  
+    if (!suppliedToPubkey) {
+      console.log(`Please provide a public key to send to`);
+      process.exit(1);
+    }
+  
+    const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
+  
+    console.log(`suppliedToPubkey: ${suppliedToPubkey}`);
+  
+    const toPubkey = new PublicKey(suppliedToPubkey);
+  
+    const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+  
+    console.log(
+      `✅ Loaded our own keypair, the destination public key, and connected to Solana`
+    );
+  
+    const transaction = new Transaction();
+  
+    const LAMPORTS_TO_SEND = 5000;
+  
+    const sendSolInstruction = SystemProgram.transfer({
+      fromPubkey: senderKeypair.publicKey,
+      toPubkey,
+      lamports: LAMPORTS_TO_SEND,
+    });
+  
+    transaction.add(sendSolInstruction);
+  
+    try {
+      const signature = await sendAndConfirmTransaction(connection, transaction, [
+        senderKeypair,
+      ]);
+  
+      console.log(
+        `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `
+      );
+      console.log(`Transaction signature is ${signature}!`);
+    } catch (error: unknown) { // Explicitly marking error as unknown
+      if (error instanceof Error) {
+        console.error(`Error in transaction: ${error.message}`);
+      } else {
+        console.error('An unknown error occurred in the transaction');
+      }
+    }
+  };
+  
+  main();
+  
